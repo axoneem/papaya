4dac9d8ef5f243a0bafbe6354180cc2e33f80211
Curtis Upshall <curtisupshall@gmail.com>
2025-12-23 07:33:51 -0800

    Removes papaya-shortcut

--- packages/papaya-shortcut/src/app/page.jsx
+++
@@ -1,7 +0,0 @@
-import { redirect } from "next/navigation";
-
-const SHORTCUT_REDIRECT_URL = process.env.SHORTCUT_REDIRECT_URL;
-
-export default function Page() {
-  redirect(SHORTCUT_REDIRECT_URL);
-}
